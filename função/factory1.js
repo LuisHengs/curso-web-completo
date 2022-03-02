7b657246ef060de4b016ee32d895335d432089f6
Luis.H <luis.hengs@protonmail.com>
2022-03-01 22:20:52 -0300

    Factory é uma função que no final retorna um objeto

---
+++ função/factory1.js
@@ -0,0 +1,11 @@
+// Factory é uma função que no final ela retorna um objeto
+
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
