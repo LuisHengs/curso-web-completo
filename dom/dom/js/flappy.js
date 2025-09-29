a5973f3a6154c3ddc5e992529b793f3edb97ef16
luishengs <luis.hengs@protonmail.com>
2025-09-29 13:08:15 -0300

    continuação jogo

--- dom/dom/js/flappy.js
+++ dom/dom/js/flappy.js
@@ -104,3 +104,20 @@ function Progresso() {
     }
     this.atualizarPontos(0)
 }
+
+
+
+
+
+
+
+//const barreiras = new Barreiras(700, 1200, 200, 400)
+//const passaro = new Passaro(700)
+//const areaDoJogo = document.querySelector('[wm-flappy]')
+
+//areaDoJogo.appendChild(passaro.elemento)
+//barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+//setInterval(() => {
+//    barreiras.animar()
+//    passaro.animar()
+//}, 20)
